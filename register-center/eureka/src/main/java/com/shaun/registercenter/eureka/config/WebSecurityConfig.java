5437b55d7e2dd9fb5d75ba1c1b76add215ff2c25
shaun_zhou <uranus-neptune@hotmail.com>
2019-10-14 15:23:43 +0800

    Add register-center:eureka:Greenwich

---
+++ register-center/eureka/src/main/java/com/shaun/registercenter/eureka/config/WebSecurityConfig.java
@@ -0,0 +1,13 @@
+package com.shaun.registercenter.eureka.config;
+
+import org.springframework.context.annotation.Configuration;
+
+//@EnableWebSecurity
+//@Configuration
+//public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http.csrf().disable(); //关闭csrf
+//        super.configure(http); //开启认证
+//    }
+//}
