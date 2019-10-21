561a371aeb6f73fec4aca8b3bb506ad27fc9e977
shaun_zhou <uranus-neptune@hotmail.com>
2019-10-21 10:03:47 +0800

    Add audit-logging-library:audit4j

---
+++ audit-logging-library/audit4j/audit4j-starter/src/test/java/com/shaun/audit4jstarter/Audit4jStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shaun.audit4jstarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Audit4jStarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
